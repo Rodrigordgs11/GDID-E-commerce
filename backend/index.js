74fa64ca1abe9eaf1fbd2010999eec6ca5e04982
Rodrigo Rodrigues <rodribola.0886@gmail.com>
2025-03-13 15:16:52 +0000

    Add CORS support and refactor login functionality for improved authentication flow

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const { sequelize } = require('./src/config/database');
 
 const app = express();
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: ["http://localhost:8181"],
+        methods: "GET,POST,PUT,DELETE,OPTIONS",
+        allowedHeaders: "Content-Type,Authorization",
+        credentials: true,
+    })
+);
 
 sequelize.sync();
 
--- frontend/app/js/login.js
+++ frontend/app/js/login.js
@@ -1,29 +1,40 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    const baseUrl = window.location.origin + window.location.pathname; 
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    const state = urlParams.get("state");
+    const client_id = urlParams.get("client_id");
+    const client_secret = urlParams.get("client_secret");
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    if (code) {
 
-    const formData = new URLSearchParams();
-    formData.append('username', username);
-    formData.append('password', password);
-    formData.append('client_id', 'app');
-    formData.append('response_type', 'code');
-    formData.append('redirect_uri', 'http://localhost:3000');
-    formData.append('scope', 'read');
-    formData.append('state', 'xwz');
+        const data = {
+            code,
+            // state,
+            client_id: client_id,
+            client_secret: client_secret,
+            redirect_uri: baseUrl,
+        };
 
-    try {
-        const response = await fetch('http://localhost:3000/authorize', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: formData
-        });
-
-        if (!response.ok) throw new Error('Falha no login');
-        const data = await response.json();
-        console.log('Código de autorização:', data);
-    } catch (error) {
-        document.getElementById('errorMessage').innerText = 'Erro: ' + error.message;
+        fetch("http://localhost:3001/exchange-code-for-token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    alert("Erro ao realizar login");
+                } else {
+                    //localStorage.setItem("token", data.token);
+                    window.location.href = "http://localhost:8181/";
+                }
+            })
+            .catch((error) => {
+                console.error("Erro ao realizar login:", error);
+                alert("Erro ao realizar login");
+            });
     }
 });
