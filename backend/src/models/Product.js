a690978f995f7082bb7e72b5e7ef8648fd24bb77
Pedro Silva <pedrosedry@gmail.com>
2025-03-14 21:06:02 +0000

    Refactor models to use UUIDs as primary keys; update seeding logic for roles, users, and products

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/database");
 
 const Product = sequelize.define("Product", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
     name: { type: DataTypes.STRING, allowNull: false },
     price: { type: DataTypes.FLOAT, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
